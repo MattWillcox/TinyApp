9006800c7cde5e3b4d6b7ce5c62c190f819773b9
Matt Willcox <mattjwillcox@gmail.com>
2017-08-01 20:12:21 +0000

    Adds body-parser library

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-var PORT = process.env.PORT || 8080; // default port 8080
+const PORT = process.env.PORT || 8080; // default port 8080
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
